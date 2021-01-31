b91ef5bf86335e156cdad39a9bc553c4490bc37b
kkxxll <550893685@qq.com>
2021-01-31 15:39:36 +0800

    prefetch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 document.addEventListener('click', () => {
     // 异步调用，在click时，才会加载func函数的内容
-    import ('./click').then(({default: func}) => {
+    import (/* webpackPrefetch: true */ './click').then(({default: func}) => {
         func()
     })
 })
