9c6f93fa2499a1289fa9db9a15e63d9c4c0b6f18
kkxxll <550893685@qq.com>
2021-01-31 12:31:05 +0800

    懒加载

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-function getComponent() {
-    return import(/* webpackChunkName:"lodash" */'lodash').then(({default: _}) => {
-        var element = document.createElement('div')
-        element.innerHTML = _.join(['1', '2'], '***')
-        return element
-    })
+async function getComponent() {
+    const {default: _} = await import(/* webpackChunkName:"lodash" */'lodash')
+    var element = document.createElement('div')
+    element.innerHTML = _.join(['1', '2'], '***')
+    return element
 }
-
+// 懒加载
+// 通过click绑定getComponent方法，使得lodash的代码不会在首页加载，而是点击事件发生之后加载
 document.addEventListener('click', () => {
     getComponent().then(element => {
         document.body.appendChild(element)
