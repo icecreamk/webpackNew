ba853937a9d2cd9f6d0011f877b3ce33d0ecde5e
kkxxll <550893685@qq.com>
2021-01-31 12:28:15 +0800

    fix import

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 function getComponent() {
-    return import(/* webpackChunkName:"lodash" */'lodash').then((_) => {
+    return import(/* webpackChunkName:"lodash" */'lodash').then(({default: _}) => {
         var element = document.createElement('div')
         element.innerHTML = _.join(['1', '2'], '***')
         return element
     })
 }
 
-getComponent().then(element => {
-    document.body.appendChild(element)
+document.addEventListener('click', () => {
+    getComponent().then(element => {
+        document.body.appendChild(element)
+    })
 })
