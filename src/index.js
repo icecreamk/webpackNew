b5bca413d213bc41c92b3858f78e5e7ba2245bb6
kkxxll <550893685@qq.com>
2021-02-06 14:30:39 +0800

    http-server 模拟服务器 跑dist目录

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
-import { ui } from './jquery.ui'
-import './style.css'
-
-document.addEventListener('click', () => {
-    // 异步调用，在click时，才会加载func函数的内容
-    import (/* webpackPrefetch: true */ './click').then(({default: func}) => {
-        func()
-    })
-})
-ui()
-console.log('content change')
+console.log('hello world')
